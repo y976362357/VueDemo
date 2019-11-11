562709d808f92bb84efeffbbe0c2d70086e82438
Toby Yang <toby.yang@ad.infosys.com>
2019-11-11 15:36:00 +0800

    Filtering Completed Tasks

--- todo/src/App.vue
+++ todo/src/App.vue
@@ -13,13 +13,22 @@
           Done
         </div>
       </div>
-      <div class="row" v-for="t in tasks" v-bind:key="t.action">
+      <div class="row" v-for="t in filteredTasks" v-bind:key="t.action">
         <div class="col">
           {{ t.action }}
         </div>
-        <div class="col-2">
+        <div class="col-2 text-center">
           <input type="checkbox" name="" v-model="t.done" class="form-check-input">
           {{ t.done }}
+        </div>
+      </div>
+      
+      <div class="row bg-secondary py-2 mt02 text-white">
+        <div class="col text-center">
+          <input type="checkbox" v-model="hideCompleted" class="form-check-input" />
+          <label class="form-check-label font-weight-bold">
+            Hide completed tasks
+          </label>
         </div>
       </div>
     </div>
@@ -38,7 +47,13 @@ export default {
         {action: 'Exercise Sql basic', done: false},
         {action: 'Write Some Vue Demo', done: true},
         {action: 'Call Toby', done: false},
-      ]
+      ],
+      hideCompleted: true
+    }
+  },
+  computed:{
+    filteredTasks(){
+      return this.hideCompleted ? this.tasks.filter(t => !t.done) : this.tasks
     }
   }
 }
