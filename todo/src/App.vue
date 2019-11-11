82dcec9262238de1c92fde96e0b08a90dce7e905
Toby Yang <toby.yang@ad.infosys.com>
2019-11-11 16:03:15 +0800

    adding the finshing touches

--- todo/src/App.vue
+++ todo/src/App.vue
@@ -3,24 +3,36 @@
     <h4 class="bg-primary text-white text-center p-2 rounded-pill">{{ name }}'s To Do List</h4>
 
     <div class="container-fluid p-4">
-      <div class="row">
-        <div class="col font-weight-bold">Task</div>
-        <div class="col-2 font-weight-bold">Done</div>
-      </div>
-      <div class="row" v-for="t in filteredTasks" v-bind:key="t.action">
-        <div class="col">{{ t.action }}</div>
-        <div class="col-2 text-center">
-          <input type="checkbox" name v-model="t.done" class="form-check-input" />
-          {{ t.done }}
+      <div class="row" v-if="filteredTasks.length == 0">
+        <div class="col text-center">
+          <b>Nothing to do</b>
         </div>
       </div>
+      <template v-else>
+        <div class="row">
+          <div class="col font-weight-bold">Task</div>
+          <div class="col-2 font-weight-bold">Done</div>
+        </div>
+        <div class="row" v-for="t in filteredTasks" v-bind:key="t.action">
+          <div class="col">{{ t.action }}</div>
+          <div class="col-2 text-center">
+            <input type="checkbox" name v-model="t.done" class="form-check-input" />
+            {{ t.done }}
+          </div>
+        </div>
+      </template>
 
       <div class="row py-2">
         <div class="col">
-          <input type="text" v-model="newItemText" class="form-control" v-on:keyup.enter="addNewTodo"/>
+          <input
+            type="text"
+            v-model="newItemText"
+            class="form-control"
+            v-on:keyup.enter="addNewTodo"
+          />
         </div>
         <div class="col-2">
-          <button class="btn btn-primary" v-on:click="addNewTodo" >Add</button>
+          <button class="btn btn-primary" v-on:click="addNewTodo">Add</button>
         </div>
       </div>
 
@@ -28,6 +40,12 @@
         <div class="col text-center">
           <input type="checkbox" v-model="hideCompleted" class="form-check-input" />
           <label class="form-check-label font-weight-bold">Hide completed tasks</label>
+        </div>
+
+        <div class="col text-center">
+          <button class="btn btn-sm btn-warning" v-on:click="deleteCompleted">
+            Delete Completed
+          </button>
         </div>
       </div>
     </div>
@@ -40,12 +58,7 @@ export default {
   data() {
     return {
       name: "Toby",
-      tasks: [
-        { action: "Buy book", done: false },
-        { action: "Exercise Sql basic", done: false },
-        { action: "Write Some Vue Demo", done: true },
-        { action: "Call Toby", done: false }
-      ],
+      tasks: [],
       hideCompleted: true,
       newItemText: ""
     };
@@ -61,7 +74,21 @@ export default {
         action: this.newItemText,
         done: false
       });
+      this.storeData()
       this.newItemText = "";
+    },
+    deleteCompleted(){
+      this.tasks = this.tasks.filter(t => !t.done)
+      this.storeData()
+    },
+    storeData(){
+      localStorage.setItem("todos", JSON.stringify(this.tasks));
+    }
+  },
+  created() {
+    let data = localStorage.getItem("todos");
+    if (data != null) {
+      this.tasks = JSON.parse(data);
     }
   }
 };
