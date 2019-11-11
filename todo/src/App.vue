9fa59dbcda15630b6c105681c10a6a006b1dab82
Toby Yang <toby.yang@ad.infosys.com>
2019-11-11 15:45:55 +0800

    Createing New Tasks

--- todo/src/App.vue
+++ todo/src/App.vue
@@ -1,34 +1,33 @@
 <template>
   <div id="app">
-    <h4 class="bg-primary text-white text-center p-2 rounded-pill">
-      {{ name }}'s To Do List
-    </h4>
+    <h4 class="bg-primary text-white text-center p-2 rounded-pill">{{ name }}'s To Do List</h4>
 
     <div class="container-fluid p-4">
       <div class="row">
-        <div class="col font-weight-bold">
-          Task  
-        </div>
-        <div class="col-2 font-weight-bold">
-          Done
-        </div>
+        <div class="col font-weight-bold">Task</div>
+        <div class="col-2 font-weight-bold">Done</div>
       </div>
       <div class="row" v-for="t in filteredTasks" v-bind:key="t.action">
-        <div class="col">
-          {{ t.action }}
-        </div>
+        <div class="col">{{ t.action }}</div>
         <div class="col-2 text-center">
-          <input type="checkbox" name="" v-model="t.done" class="form-check-input">
+          <input type="checkbox" name v-model="t.done" class="form-check-input" />
           {{ t.done }}
         </div>
       </div>
-      
+
+      <div class="row py-2">
+        <div class="col">
+          <input type="text" v-model="newItemText" class="form-control" v-on:keyup.enter="addNewTodo"/>
+        </div>
+        <div class="col-2">
+          <button class="btn btn-primary" v-on:click="addNewTodo" >Add</button>
+        </div>
+      </div>
+
       <div class="row bg-secondary py-2 mt02 text-white">
         <div class="col text-center">
           <input type="checkbox" v-model="hideCompleted" class="form-check-input" />
-          <label class="form-check-label font-weight-bold">
-            Hide completed tasks
-          </label>
+          <label class="form-check-label font-weight-bold">Hide completed tasks</label>
         </div>
       </div>
     </div>
@@ -36,25 +35,34 @@
 </template>
 
 <script>
-
 export default {
-  name: 'app',
-  data(){
+  name: "app",
+  data() {
     return {
-      name: 'Toby',
+      name: "Toby",
       tasks: [
-        {action: 'Buy book', done: false},
-        {action: 'Exercise Sql basic', done: false},
-        {action: 'Write Some Vue Demo', done: true},
-        {action: 'Call Toby', done: false},
+        { action: "Buy book", done: false },
+        { action: "Exercise Sql basic", done: false },
+        { action: "Write Some Vue Demo", done: true },
+        { action: "Call Toby", done: false }
       ],
-      hideCompleted: true
+      hideCompleted: true,
+      newItemText: ""
+    };
+  },
+  computed: {
+    filteredTasks() {
+      return this.hideCompleted ? this.tasks.filter(t => !t.done) : this.tasks;
     }
   },
-  computed:{
-    filteredTasks(){
-      return this.hideCompleted ? this.tasks.filter(t => !t.done) : this.tasks
+  methods: {
+    addNewTodo() {
+      this.tasks.push({
+        action: this.newItemText,
+        done: false
+      });
+      this.newItemText = "";
     }
   }
-}
+};
 </script>
