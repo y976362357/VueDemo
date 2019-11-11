637b295c43cd9cbc7def500c1be75090a80e3ff5
Toby Yang <toby.yang@ad.infosys.com>
2019-11-11 15:19:28 +0800

    Displaying the list of the Tasks

--- todo/src/App.vue
+++ todo/src/App.vue
@@ -1,14 +1,44 @@
 <template>
   <div id="app">
-    <h4 class="bg-primary text-white text-center p-2">
-      To Do List
+    <h4 class="bg-primary text-white text-center p-2 rounded-pill">
+      {{ name }}'s To Do List
     </h4>
+
+    <div class="container-fluid p-4">
+      <div class="row">
+        <div class="col font-weight-bold">
+          Task  
+        </div>
+        <div class="col-2 font-weight-bold">
+          Done
+        </div>
+      </div>
+      <div class="row" v-for="t in tasks" v-bind:key="t.action">
+        <div class="col">
+          {{ t.action }}
+        </div>
+        <div class="col-2">
+          {{ t.done }}
+        </div>
+      </div>
+    </div>
   </div>
 </template>
 
 <script>
 
 export default {
-  name: 'app'
+  name: 'app',
+  data(){
+    return {
+      name: 'Toby',
+      tasks: [
+        {action: 'Buy book', done: false},
+        {action: 'Exercise Sql basic', done: false},
+        {action: 'Write Some Vue Demo', done: false},
+        {action: 'Call Toby', done: false},
+      ]
+    }
+  }
 }
 </script>
